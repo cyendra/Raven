7cf43f7977c2bf8125bf248672cbb3ed1d15891e
cyendra <g1483335912@gmail.com>
2014-09-26 15:02:57 +0800

    Env修正

--- Raven/Environment.cpp
+++ Raven/Environment.cpp
@@ -6,8 +6,28 @@ namespace RavenInternal {
 	EnvNode
 	***************************************************************************/
 
-	EnvNode::EnvNode() { }
+	EnvNode::EnvNode() : Prev(nullptr) { }
+	
 	EnvNode::~EnvNode() { }
+	
+	void EnvNode::Init() {
+		dict.clear();
+	}
+
+	std::shared_ptr<Value> EnvNode::GetValue(std::string name) {
+		auto it = dict.find(name);
+		if (it == dict.end()) return std::shared_ptr<Value>(new Value());
+		return it->second;
+	}
+
+	bool EnvNode::HasVar(std::string name) {
+		auto it = dict.find(name);
+		return it != dict.end();
+	}
+
+	void EnvNode::SetValue(std::string name, std::shared_ptr<Value>value) {
+		dict[name] = value;
+	}
 
 	/***************************************************************************
 	Environment
@@ -17,20 +37,51 @@ namespace RavenInternal {
 
 	Environment::~Environment() { }
 
-	void Environment::NewEnv() { }
+	void Environment::NewEnv() {
+		auto p = std::shared_ptr<EnvNode>(new EnvNode());
+		p->Prev = node;
+		node = p;
+	}
 
-	void Environment::PopEnv() { }
+	void Environment::PopEnv() { 
+		node = node->Prev;
+	}
 
 	std::shared_ptr<Value> Environment::GetIdValue(std::shared_ptr<Token> tok) {
-
+		auto name = tok->GetText();
+		auto p = node;
+		while (p != nullptr) {
+			if (p->HasVar(name)) {
+				return p->GetValue(name);
+			}
+			else {
+				p = p->Prev;
+			}
+		}
+		return std::shared_ptr<Value>(new Value());
 	}
 
 	void Environment::SetIdValue(std::shared_ptr<Token> tok, std::shared_ptr<Value> v) {
+		auto name = tok->GetText();
+		auto p = node;
+		while (p != nullptr) {
+			if (p->HasVar(name)) {
+				return p->SetValue(name, v);
+			}
+			else {
+				p = p->Prev;
+			}
+		}
+	}
 
+	bool Environment::FindId(std::shared_ptr<Token> tok) {
+		auto name = tok->GetText();
+		return node->HasVar(name);
 	}
 
 	void Environment::RegistId(std::shared_ptr<Token> tok) {
-
+		auto name = tok->GetText();
+		node->SetValue(name, std::shared_ptr<Value>(new Value()));
 	}
 
 }
--- Raven/Environment.h
+++ Raven/Environment.h
@@ -21,6 +21,13 @@ namespace RavenInternal {
 	public:
 		EnvNode();
 		~EnvNode();
+		std::shared_ptr<Value> GetValue(std::string name);
+		bool HasVar(std::string name);
+		void SetValue(std::string, std::shared_ptr<Value>value);
+		void Init();
+		std::shared_ptr<EnvNode> Prev;
+	private:
+		std::map<std::string, std::shared_ptr<Value>> dict;
 	};
 
 	/***************************************************************************
@@ -35,7 +42,10 @@ namespace RavenInternal {
 		void PopEnv();
 		std::shared_ptr<Value> GetIdValue(std::shared_ptr<Token> tok);
 		void SetIdValue(std::shared_ptr<Token> tok, std::shared_ptr<Value> v);
+		bool FindId(std::shared_ptr<Token> tok);
 		void RegistId(std::shared_ptr<Token> tok);
+	private:
+		std::shared_ptr<EnvNode> node;
 	};
 
 }
